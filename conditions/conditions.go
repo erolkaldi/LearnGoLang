2cf26054fc08b05f3655e519230514e02e1cb477
erolkaldi <43037084+erolkaldi@users.noreply.github.com>
2022-10-31 14:26:12 +0300

    Conditions

--- conditions/conditions.go
+++ conditions/conditions.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	x, y := 5, 3
+
+	fmt.Println(x, y)
+	fmt.Printf("%T, %v\n", x == y, x == y)
+
+	if x > y {
+		fmt.Println("If condition returned ", x > y)
+	}
+
+	if x > y {
+		fmt.Println("Greater")
+	} else if x == y {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Smaller")
+	}
+
+	a, b := "A", "B"
+
+	fmt.Println(a != b)
 }
