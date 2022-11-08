aa6108b53ca4542a7e08b1d1a1f9cd70bac4b8d0
erolkaldi <43037084+erolkaldi@users.noreply.github.com>
2022-11-08 16:54:52 +0300

    Api CRUD

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,9 +23,62 @@ var community = []person{
 func getPeople(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, community)
 }
+
+func createPerson(c *gin.Context) {
+	var newPerson person
+	if err := c.BindJSON(&newPerson); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	exists := false
+	for _, p := range community {
+		if p.Name == newPerson.Name {
+			exists = true
+			break
+		}
+	}
+	if exists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Person already exists"})
+		return
+	}
+	newPerson.ID = community[len(community)-1].ID + 1
+	community = append(community, newPerson)
+	c.IndentedJSON(http.StatusCreated, newPerson)
+}
+
+func getPersonById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	for _, p := range community {
+		if p.ID == id {
+			c.IndentedJSON(http.StatusOK, p)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Person not found"})
+
+}
+func deletePersonById(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	idx := 0
+	for i, p := range community {
+		if p.ID == id {
+			idx = i
+		}
+	}
+	if idx != 0 {
+		community = append(community[:idx], community[idx+1:]...)
+		c.IndentedJSON(http.StatusOK, gin.H{"message": "Person removed"})
+		return
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Person not found"})
+
+}
 func main() {
 	router := gin.Default()
 	router.GET("/people", getPeople)
+	router.GET("/people/:id", getPersonById)
+	router.POST("/people", createPerson)
+	router.DELETE("/people/:id", deletePersonById)
 	router.Run(":8080")
 
 }
